b1f006b6271a29a67deaa9b1b73189556024cb7c
Liliyalexx <liliya.fedyurina@gmail.com>
2023-03-31 09:21:26 -0700

    src changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
-// import Home from './components/Home'
-// import About from './components/About'
-// import Contact from './components/Contact'
 import Layout from './components/Layout'
-// import Portfolio from './components/Portfolio'
-// import Dashboard from './components/Dashboard'
-
 
 function App() {
   return (
